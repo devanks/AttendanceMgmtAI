c1bb44634eebb56e8290fab715cdde2ba68c4bd5
Devan Sambasivan <32042038+devanks@users.noreply.github.com>
2019-04-27 12:08:36 +0530

    Incomplete code for session

--- Backend/FaceRecog/forms.py
+++ Backend/FaceRecog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.contrib.admin import widgets
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser, TeacherTeachesSubject, StudentTookClass
+from .models import CustomUser, TeacherTeachesSubject, StudentTookClass, SessionRecord, AttendanceRecord
+
 from bootstrap_datepicker_plus import DateTimePickerInput
 
 
@@ -20,6 +22,19 @@ class chooseSubject(forms.ModelForm):
     class Meta:
         model = TeacherTeachesSubject
         fields = ["subject"]
+
+class createSession(forms.ModelForm):
+    class Meta:
+        model = SessionRecord
+        fields = ["Teacher_Teaches_Subject","DateOfClass"]
+        widgets = {
+            'DateOfClass': 	DateTimePickerInput(), 
+        }
+
+class chooseSession(forms.ModelForm):
+    class Meta:
+        model = AttendanceRecord
+        fields = ["session"]
 
 class checkAttendanceOfSubject(forms.ModelForm):
     class Meta:
